f8feb2a05ab3d1c8ba64381763585683c75bc00f
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2022-04-20 19:50:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//==============================================================================================================
+//                                         Database Sync Project
+//==============================================================================================================
 
+// Title : Database Sync Project
+// Author: Jun Kim
+// Date: 04/2022
+// Description: In this project, a synchronize() method is implemented that
+// copies all records from a sourceDb into a targetDb() then start polling for changes.
+
+const the = require('await-the');
+const {initializeDatabases,getDatabases,load} = require("./database.js");
+const {synchronize, syncAllNoLimit, syncWithLimit, syncAllSafely,syncAllSafelyNoRemove, syncNewChanges, touch, readSource,readTarget} = require("./syncFunc.js");
+
+                                                               // Below Code shows a few tests I made before using mocha,
+//Initialize Database and Load Data                            
+
+initializeDatabases()                                         
+load()                                                         
+                                                               // Refer to test.js for official testing
