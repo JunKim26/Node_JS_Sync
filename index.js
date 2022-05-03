6b15bcfb47bff719438fbf227982c40ac9c4b1fb
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2022-05-03 10:35:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Title : Database Sync Project
-// Author: Jun Kim
-// Date: 04/2022
-// Description: In this project, a synchronize() method is implemented that
-// copies all records from a sourceDb into a targetDb() then start polling for changes.
+/*
+Title : Database Sync Project
+Author: Jun Kim
+Date: 04/2022
+Description: In this project, a synchronize() method is implemented that copies all records 
+from a sourceDb into a targetDb() then start polling for changes.
+*/
 
 const the = require('await-the');
 const {initializeDatabases,getDatabases,load} = require("./database.js");
