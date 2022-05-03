fda7e87943b1d370c1d418fe395ccd3adb3be777
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2022-05-03 10:06:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//==============================================================================================================
-//                                         Database Sync Project
-//==============================================================================================================
-
 // Title : Database Sync Project
 // Author: Jun Kim
 // Date: 04/2022
@@ -12,9 +8,9 @@ const the = require('await-the');
 const {initializeDatabases,getDatabases,load} = require("./database.js");
 const {synchronize, syncAllNoLimit, syncWithLimit, syncAllSafely,syncAllSafelyNoRemove, syncNewChanges, touch, readSource,readTarget} = require("./syncFunc.js");
 
-                                                               // Below Code shows a few tests I made before using mocha,
+                                                          
 //Initialize Database and Load Data                            
-
 initializeDatabases()                                         
-load()                                                         
-                                                               // Refer to test.js for official testing
+load()                  
+
+// Refer to test.js for official testing
